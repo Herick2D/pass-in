4032d731d062d46e784478cdecb532f2c7f860c3
Herick2D <contato.herickmoreira@gmail.com>
2024-04-03 10:26:27 -0300

    feat: POST/check-in endpoint and business rules

--- src/main/java/com/herick/passin/controllers/AttendeeController.java
+++ src/main/java/com/herick/passin/controllers/AttendeeController.java
@@ -2,12 +2,10 @@ package com.herick.passin.controllers;
 
 import com.herick.passin.dto.attendee.AttendeeBadgeResponseDTO;
 import com.herick.passin.services.AttendeeService;
+import com.herick.passin.services.CheckInService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -20,6 +18,16 @@ public class AttendeeController {
   public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
     AttendeeBadgeResponseDTO response =  this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
     return ResponseEntity.ok(response);
+  }
+
+  @PostMapping("/{attendeeId}/check-in")
+  public ResponseEntity registerCheckIn(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+    this.attendeeService.checkInAttendee(attendeeId);
+
+    var uri = uriComponentsBuilder.path("/attendees/{attendeeId}/badge").buildAndExpand(attendeeId).toUri();
+
+    return ResponseEntity.created(uri).build();
+
   }
 
 }
